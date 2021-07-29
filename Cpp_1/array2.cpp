554fad7b22d4f9ab30631ffafe92852288dd7559
huangjian <huangjian@ft.com>
2021-07-29 19:19:13 +0800

    m

---
+++ Cpp_1/array2.cpp
@@ -0,0 +1,37 @@
+//
+// Created by huangjian on 7/29/21.
+//
+
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+using std::setw;
+
+/*
+ * 数组指针指向数组中第一个元素的地址
+ */
+
+/*
+int main() {
+    int array[10];
+
+    for (int i = 0; i < 10; ++i) {
+        array[i] = 100 + i;
+    }
+
+    */
+/*
+     * setw( 13 )    13位右对齐填充后面的"Value", 不足位用空格填充
+     * 即输出："Element{8个空格}Value"
+     *//*
+
+    cout << "Element" << setw( 13 ) << "Value" << endl;
+
+    for (int j = 0; j < 10; j++) {
+        cout << setw(7)<< j << setw(13) << array[j] << endl;
+    }
+
+    return 0;
+}
+*/
---
+++ Cpp_1/rand.cpp
@@ -0,0 +1,20 @@
+//
+// Created by huangjian on 7/29/21.
+//
+
+#include <iostream>
+
+using namespace std;
+
+/*
+int main() {
+    // 设置种子
+    srand((unsigned)time(NULL));
+
+    for (int i = 0; i < 10; i++ ) {
+        cout <<"随机数： " << rand() << endl;
+    }
+
+    return 0;
+}
+*/
